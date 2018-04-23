5c23a56e664ee40b62538b3cdd588bec0d5ac8d9
Eric Range <ericrange@users.noreply.github.com>
2018-04-23 16:09:10 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+    #
+#   # #   Enginsight GmbH
+# # # #   Geschäftsführer: Mario Jandeck, Eric Range
+# #   #   Hans-Knöll-Straße 6, 07745 Jena
+  #   
+
+/usr/bin/apt-get install firewalld -y
+
+/bin/systemctl enabled firewalld
+/bin/systemctl start firewalld
+
+/usr/bin/firewall-cmd --zone=public --add-service=ssh
+/usr/bin/firewall-cmd --zone=public --permanent --add-service=ssh
+
+/usr/bin/firewall-cmd --reload
